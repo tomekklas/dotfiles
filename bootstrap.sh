c64259c14161783e2d70c348111f7aec734d1a29
Tomek Klas <github@tomekklas.com>
2018-09-25 23:09:00 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,3 +26,10 @@ else
 fi;
 unset doIt;
 
+# install AWS CLI
+cd /tmp/
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+
